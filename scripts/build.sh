6f00ee8e7f0fd1aec92826089da1e99193be423f
Hemant2A2 <hemant.ayuj@gmail.com>
2025-03-25 20:29:47 +0530

    working on linux

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
 
-PROJECT_DIR="/Users/hemantayuj/Desktop/compiler/Groquaza/src"
-COMPILER="g++-14"
-STD_VERSION="-std=c++20"
+OS_NAME=$(uname -s)
+if [[ "$OS_NAME" == "Darwin" ]]; then
+    PROJECT_DIR="/Users/hemantayuj/Desktop/compiler/Groquaza/src"
+    STD_VERSION="-std=c++20"
+elif [[ "$OS_NAME" == "Linux" ]]; then
+    PROJECT_DIR="/home/h3m/Desktop/Projects/Groquaza-compiler/src"
+    STD_VERSION="-std=c++2a"
+else 
+    echo "Unsupported Os"
+    exit 1
+fi
+
+COMPILER=$(which g++-14 || which g++ || echo "nope")
+if [[ "$COMPILER" == "nope" ]]; then
+    echo "g++ not found"
+    exit 1
+fi
+
 SRC_FILES="main.cpp lexer/Lexer.cpp lexer/reader/Reader.cpp lexer/reader/CodeLoc.cpp parser/Parser.cpp ast/PrintAST.cpp code-generator/AsmEmitter.cpp code-generator/CodeGenerator.cpp runtime/runtime.c symbol-table/SymbolTable.cpp"
 OUTPUT="main"
 
---
+++ scripts/generate.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+ARCH=$(uname -m)
+if [[ "$ARCH" != "aarch64" ]]; then
+    echo "Only works for Aarch64 assembly"
+    exit 1
+fi
+
+PROJECT_DIR="/home/h3m/Desktop/Projects/Groquaza-compiler/src"
+TEST_FILE="sum"
+
+cd "$PROJECT_DIR" || { echo "Project directory not found"; exit 1; }
+
+as -o output_"$TEST_FILE".o output_"$TEST_FILE".s || { echo "Assembly failed"; exit 1; }
+gcc -c runtime/runtime.c -o runtime.o -std=c11 || { echo "Runtime compilation failed"; exit 1; }
+gcc -o output_"$TEST_FILE" output_"$TEST_FILE".o runtime.o -nostartfiles -lc || { echo "Linking failed"; exit 1; }
+
+./output_"$TEST_FILE"
